818a4c6cfe817d15d19d8cf4923a64fcab9127aa
Max <beppe@live.se>
2022-08-25 17:56:53 +1000

    feat: add some examples

--- app/lib/swell.ts
+++ app/lib/swell.ts
@@ -1,11 +1,72 @@
 import swell from 'swell-js'
 
+interface Credentials {
+  storeId: string;
+  publicKey: string;
+}
+
+interface FetchProductsParams {
+  limit?: number;
+  page?: number;
+}
+
 const options = {
   useCamelCase: true
 };
 
-export function getSwell(storeId: string, publicKey: string) {
-  swell.init(storeId, publicKey, options)
-  return swell
+// interface iSwell {
+//   fetchSettings(): Promise<any>;
+//   fetchProducts(options?: FetchProductsParams): Promise<any>;
+// }
+
+// export class Swell implements iSwell {
+//   private swellJs: any;
+//   constructor(storeId: string, publicKey: string) {
+//     swell.init(storeId, publicKey, options);
+//     this.swellJs = swell;
+//   }
+
+//   async fetchSettings() {
+//     return this.swellJs.settings.get();
+//   }
+
+//   async fetchProducts(options = {limit: 25, page: 1}) {
+//     return this.swellJs.products.list(options);
+//   }
+// }
+
+export function initSwell(storeId: string, publicKey: string) {
+  if (!swell.options?.store && !swell.options?.key)
+    swell.init(storeId, publicKey, options);
 }
 
+const ensureSwellIsInitialised = (fn: any) => (...params: any) => {
+  if (!swell.options?.store && !swell.options?.key) {
+    throw new Error('Swell is not initialised, please ensure to initialise Swell');
+  }
+  return fn(...params);
+}
+
+export const fetchSettings = ensureSwellIsInitialised(
+  async function fetchSettings() {
+    return swell.settings.get();
+  }
+)
+
+export const fetchProducts = ensureSwellIsInitialised(
+  async function fetchProducts(options: FetchProductsParams = {limit: 25, page: 1}) {
+    return swell.products.list(options);
+  }
+)
+
+export const fetchProduct = ensureSwellIsInitialised(
+  async function fetchProduct(slugOrId: string) {
+    return swell.products.get(slugOrId);
+  }
+)
+
+export const fetchCart = ensureSwellIsInitialised(
+  async function fetchCart() {
+    return swell.cart.get();
+  }
+)
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,29 +1,37 @@
 import { useLoaderData } from "@remix-run/react";
-import { getSwell } from "~/lib/swell";
+import { fetchCart, fetchProducts, fetchSettings, initSwell } from "~/lib/swell";
 
 export async function loader({context}) {
   const swellStoreId = context.SWELL_STORE_ID;
   const swellPublicKey = context.SWELL_PUBLIC_KEY;
-  const swell = getSwell(swellStoreId, swellPublicKey);
-  const [settings, products] = await Promise.all([
-    swell.settings.get(),
-    swell.products.get()
+
+  initSwell(swellStoreId, swellPublicKey)
+
+  const [settings, products, cart] = await Promise.all([
+    fetchSettings(),
+    fetchProducts(),
+    fetchCart(),
   ])
-  console.log('settings :>> ', settings);
-  console.log('products :>> ', products);
   return {
     settings,
-    products
+    products,
+    cart,
   }
 }
 
 export default function Index() {
-  const {settings, products} = useLoaderData()
+  const {settings, products} = useLoaderData();
+  console.log('products :>> ', products);
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1 className="text-3xl font-bold underline">Welcome to {settings.store.name}</h1>
       <p>We have {products.count} products in our fantastic store, have a look at them all below.</p>
-      {products.results.map((product: any) => (<p key={product.id}>{product.name}</p>))}
+      {products.results.map((product: any) => (
+        <div key={product.id}>
+          <img alt={product.name} src={product.images[0].file.url} />
+          <p >{product.name}</p>
+        </div>
+      ))}
     </div>
   );
 }
