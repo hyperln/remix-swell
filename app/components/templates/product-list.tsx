65e98106cdcafb4e80038ad97b1143f5872a9394
Christian <christian.torlegard@outlook.com>
2022-09-01 22:02:22 +0200

    WIP: trying to implement prodcut-model

--- app/components/templates/product-list.tsx
+++ app/components/templates/product-list.tsx
@@ -1,4 +1,3 @@
-
 import { ProductItem } from '../organisms/product-item';
 interface Props {
   products: Product[];
@@ -12,28 +11,21 @@ interface Product {
   images: {
     file: {
       url: string;
-    }
+    };
   }[];
 }
 
 export function ProductList({ products }: Props) {
-
-
   return (
-    <div className="bg-white">
-      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h2 className="sr-only">Products</h2>
-        <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+    <div className='bg-white'>
+      <div className='max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8'>
+        <h2 className='sr-only'>Products</h2>
+        <div className='grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8'>
           {products.map((product: Product) => (
-            <ProductItem
-              key={product.id} product={product}
-            />
+            <ProductItem key={product.id} product={product} />
           ))}
         </div>
       </div>
     </div>
-
   );
 }
-
-
--- app/routes/product.tsx
+++ app/routes/product.tsx
@@ -1,10 +1,113 @@
-import { Outlet } from "@remix-run/react";
+import { Outlet, useLoaderData } from '@remix-run/react';
+import { useState } from 'react';
+import { StarIcon } from '@heroicons/react/20/solid';
+import { RadioGroup } from '@headlessui/react';
+import { CurrencyDollarIcon, GlobeAmericasIcon } from '@heroicons/react/24/outline';
+import { ProductItem } from '../organisms/product-item';
+import { fetchProducts } from '~/lib/swell';
+import { json } from '@remix-run/cloudflare';
+import { useParams } from '@remix-run/react';
 
-export default function product() {
-    return (
-        <div>
-            <h1>Product</h1>
-            <Outlet />
+interface Props {
+  product: Product;
+}
+interface Product {
+  id: string;
+  href: string;
+  name: string;
+  price: string;
+  description: string;
+  images: {
+    file: {
+      url: string;
+    };
+  }[];
+
+  //   details: [
+  //     'Only the best materials',
+  //     'Ethically and locally made',
+  //     'Pre-washed and pre-shrunk',
+  //     'Machine wash cold with similar colors'
+  //   ];
+}
+
+// function classNames(...classes) {
+//   return classes.filter(Boolean).join(' ');
+// }
+
+// }
+// export const loader = async () => {
+//     return json(await fetchProducts());
+// };
+// const data = useLoaderData();
+//     return (
+//       <ul>
+//         {data.map((product) => (
+//           <li key={product.id}>{product.name}</li>
+//         ))}
+//       </ul>
+//     );
+
+// export function loader({ params } : any) {
+//     const id = params.productId;
+//   }
+
+//   export function action({ params }: any) {
+//     const id = params.productId;
+//   }
+
+export default function productPage({ product }: Props) {
+  return (
+    <a key={product.id} href={product.href} className='group'>
+      <div className='bg-white'>
+        <div className='pt-6 pb-16 sm:pb-24'>
+          <div className='mx-auto mt-8 max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8'>
+            <div className='lg:grid lg:auto-rows-min lg:grid-cols-12 lg:gap-x-8'>
+              <div className='lg:col-span-5 lg:col-start-8'>
+                <div className='flex justify-between'>
+                  <h1 className='text-xl font-medium text-gray-900'>{product.name}</h1>
+                  <p className='text-xl font-medium text-gray-900'>{product.price}</p>
+                </div>
+
+                <div className='mt-8 lg:col-span-7 lg:col-start-1 lg:row-span-3 lg:row-start-1 lg:mt-0'>
+                  <h2 className='sr-only'>Images</h2>
+
+                  <div className='grid grid-cols-1 lg:grid-cols-2 lg:grid-rows-3 lg:gap-8'>
+                    <img key={product.id} src={imageUrl} alt={product.name} />
+
+                    {/* {product.image.map((image) => (
+                      <img
+                        key={product.id}
+                        src={imageUrl}
+                        alt={product.name}
+                        className='w-full h-full object-center object-cover group-hover:opacity-75'
+                      />
+                    ))} */}
+                  </div>
+                </div>
+              </div>
+              <div className='mt-8 lg:col-span-5'>
+                <div className='mt-10'>
+                  <h2 className='text-sm font-medium text-gray-900'>Description</h2>
+
+                  <div
+                    className='prose prose-sm mt-4 text-gray-500'
+                    dangerouslySetInnerHTML={{ __html: product.description }}
+                  />
+                </div>
+
+                <div className='prose prose-sm mt-4 text-gray-500'>
+                  <ul role='list'>
+                    {product.details.map((item) => (
+                      <li key={item}>{item}</li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </a>
+  );
 }
