9a340aeb1c3520ad3d17c9b1061b85d0834c4f62
Max <beppe@live.se>
2022-09-02 23:27:28 +1000

    fetch product

--- app/routes/products/$slug.tsx
+++ app/routes/products/$slug.tsx
@@ -1,11 +1,27 @@
-import { useParams } from '@remix-run/react';
+import { useLoaderData, useParams } from '@remix-run/react';
+import { fetchProduct, initSwell } from '~/lib/swell';
+
+export async function loader({params, context}) {
+  const swellStoreId = context.SWELL_STORE_ID;
+  const swellPublicKey = context.SWELL_PUBLIC_KEY;
+
+  initSwell(swellStoreId, swellPublicKey)
+
+
+  const product = await fetchProduct(params.slug)
+
+  return {
+    product
+  }
+}
 
 export default function ProductPage() {
-  const params = useParams();
-  console.log(params);
+  const { product } = useLoaderData();
+  console.log('product :>> ', product);
   return (
     <div>
-      <h3>Product id {JSON.stringify(params)}</h3>
+      <h3>Product {product?.name}</h3>
+      <img src={product.images[0].file.url} alt={product.name} />
     </div>
   );
 }
