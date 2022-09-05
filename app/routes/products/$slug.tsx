a0e747908060f1e1115c2c634ef9c4fe055d08b2
Christian <christian.torlegard@outlook.com>
2022-09-05 12:17:17 +0200

    refactor: removed unused code and import.

--- app/routes/products/$slug.tsx
+++ app/routes/products/$slug.tsx
@@ -1,23 +1,21 @@
-import { useLoaderData, useParams } from '@remix-run/react';
+import { useLoaderData } from '@remix-run/react';
 import { fetchProduct, initSwell } from '~/lib/swell';
 
-export async function loader({params, context}) {
+export async function loader({ params, context }) {
   const swellStoreId = context.SWELL_STORE_ID;
   const swellPublicKey = context.SWELL_PUBLIC_KEY;
 
-  initSwell(swellStoreId, swellPublicKey)
-
-
-  const product = await fetchProduct(params.slug)
+  initSwell(swellStoreId, swellPublicKey);
+  const product = await fetchProduct(params.slug);
 
   return {
-    product
-  }
+    product,
+  };
 }
 
 export default function ProductPage() {
   const { product } = useLoaderData();
-  console.log('product :>> ', product);
+
   return (
     <div>
       <h3>Product {product?.name}</h3>
