4e5559fb1950dae55a814ad9b7d5f955433d24f3
Christian <christian.torlegard@outlook.com>
2022-09-16 12:48:31 +0200

    refactor: bs

--- app/components/organisms/product-item.tsx
+++ app/components/organisms/product-item.tsx
@@ -1,4 +1,5 @@
-import { addItemToCart } from "~/lib/swell";
+import { Link } from '@remix-run/react';
+import { addItemToCart } from '~/lib/swell';
 
 interface Props {
   product: Product;
@@ -12,7 +13,7 @@ interface Product {
   images: {
     file: {
       url: string;
-    }
+    };
   }[];
 }
 
@@ -21,14 +22,18 @@ export function ProductItem({ product }: Props) {
 
   return (
     <Link to={`/products/${product.slug}`}>
-      <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-        <img alt={product.name} src={imageUrl} className="w-full h-full object-center object-cover group-hover:opacity-75" />
+      <div className='w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8'>
+        <img
+          alt={product.name}
+          src={imageUrl}
+          className='w-full h-full object-center object-cover group-hover:opacity-75'
+        />
       </div>
-      <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
-      <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
+      <h3 className='mt-4 text-sm text-gray-700'>{product.name}</h3>
+      <p className='mt-1 text-lg font-medium text-gray-900'>{product.price}</p>
       <button
         onClick={() => addItemToCart({ productId: product.id })}
-        className="mt-3 flex w-full items-center justify-center rounded-md border  bg-white py-3 px-8 text-base font-medium text-black hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-offset-1"
+        className='mt-3 flex w-full items-center justify-center rounded-md border  bg-white py-3 px-8 text-base font-medium text-black hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-offset-1'
       >
         Add to cart
       </button>
